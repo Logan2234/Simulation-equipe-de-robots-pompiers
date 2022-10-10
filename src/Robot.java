c062dba778b4adb934f6a4ef8d56a8c29285d694
Jorge Luri <jorge.luri-vano@grenoble-inp.org>
2022-10-10 13:54:38 +0200

    Premiers avancements de robot

--- src/Robot.java
+++ src/Robot.java
@@ -1,3 +1,29 @@
 public abstract class Robot{
+    private Case position;
+    private int capacite;
     
+    public Robot(Case position, int capacite) {
+    	this.position = position;
+    	this.capacite = capacite;
+    }
+    
+    public Case getPosition() {
+    	return this.position;
+    }
+    
+    public void setPositon(Case position) {
+    	this.position;
+    }
+    
+    public double getVitesse(NatureTerrain nature) {
+    	// TODO
+    }
+    
+    public void deverserEau(int quantite) {
+    	//TODO
+    }
+    
+    public void remplirReservoir() {
+    	// TODO
+    }
 }
