bbe7f6b60b026b8a47dabac8b61126dec5f81e32
Logan <logan.willem@grenoble-inp.org>
2022-10-10 13:55:17 +0200

    Ajout de l'enum direction et des méthodes de Carte

--- src/Carte.java
+++ src/Carte.java
@@ -1,11 +1,33 @@
 public class Carte {
-
-    public Carte() {
+    private int tailleCases;
+    private int nbLignes;
+    private int nbColonnes;
+    
+    public Carte(int nbLignes, int nbColonnes) {
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
     }
 
-    @Override
-    public String toString() {
-        return "Carte []";
+    public int getTailleCases() {
+        return tailleCases;
+    }
+
+    public int getNbLignes() {
+        return nbLignes;
+    }
+
+    public int getNbColonnes() {
+        return nbColonnes;
+    }
+    
+    public boolean voisinExiste(Case src, Direction dir){
+        // TODO: Cette fonction est à implémenter
+        return true;
+    }
+
+    public Case getVoisin(Case src, Direction dir){
+        // TODO: Cette fonction est à implémenter
+        return null;
     }
 
 }
