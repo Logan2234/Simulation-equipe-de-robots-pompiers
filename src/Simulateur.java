c5dfcb8ff7c05b30f1d7a74ca69f22946ab2f9d6
Jorge Luri Vañó <jorge.luri@redarquiadigital.com>
2022-10-18 12:40:06 +0200

    Création de simulateur

---
+++ src/Simulateur.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Simulateur {
+    private long dateSimulation;
+    Queue<Evenement> queue = new LinkedList<>();
+
+    public Simulateur(){
+        this.dateSimulation = 0;
+    }
+
+    void incrementeDate(){
+        this.dateSimulation = this.dateSimulation + 1
+    }
+
+    void ajouteEvenement(Evenement e){
+        this.queue.add(e);
+    }
+
+    boolean simulationTerminee(){
+        if (this.queue.size() == 0){
+            return true;
+        }
+        return false;
+    }
+}
