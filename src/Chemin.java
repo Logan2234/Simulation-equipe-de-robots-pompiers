7179eb9d1f071f2fee05707e4037067c84dde68d
Logan <logan.willem@grenoble-inp.org>
2022-10-19 13:54:24 +0200

    Ajout de la notion de chemin créant une liste chainée de tuples entre une case et sa date d'arrivée

---
+++ src/Chemin.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+
+public class Chemin {
+    private LinkedList<AssociationTempsCase> chemin;
+
+    public LinkedList<AssociationTempsCase> getChemin() {
+        return chemin;
+    }
+
+    public void addElement(Case caseSuiv, int date){
+        this.chemin.add(new AssociationTempsCase(caseSuiv, date));
+    }
+
+}
+
+class AssociationTempsCase{
+    private Case _case;
+    private int t;
+    
+    public AssociationTempsCase(Case _case, int t) {
+        this._case = _case;
+        this.t = t;
+    }
+
+    public Case get_case() {
+        return _case;
+    }
+
+    public int getT() {
+        return t;
+    }
+}
