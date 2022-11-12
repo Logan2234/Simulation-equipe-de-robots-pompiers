d842f985735ec17b88993db5a5e8c0e2863c5a9c
Jorge Luri Vañó <jorge.luri@redarquiadigital.com>
2022-11-12 15:47:01 +0100

    Ajout de simulateur dans chef pompier

--- src/Donnees/ChefBasique.java
+++ src/Donnees/ChefBasique.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import Donnees.Robot.Robot;
+import Evenements.Simulateur;
 
 public class ChefBasique {
     private Carte carte;
     private DonneesSimulation donnees;
+    private Simulateur simulateur;
     private HashMap<Incendie, Robot> incendies_rob;
     private ArrayList<Robot> occupes;
 
-    public ChefBasique(Carte carte, DonneesSimulation donnees){
+    public ChefBasique(Carte carte, DonneesSimulation donnees, Simulateur simulateur){
         this.carte = carte;
         this.donnees = donnees;
+        this.simulateur = simulateur;
         this.occupes = new ArrayList<Robot>();
         this.incendies_rob = new HashMap<Incendie, Robot>();
         for (Incendie incendie :donnees.getIncendies()){
@@ -29,10 +32,15 @@ public class ChefBasique {
     public void gestionIncendies(){
         for (Incendie incendie :donnees.getIncendies()){
             Robot robotIncendie = incendies_rob.get(incendie);
+
+            // Si aucun robot est attribué : 
             if (robotIncendie == null){
                 for (Robot robot : this.donnees.getRobots()){
+
+                    // Si le robot qu'on cherche à attribué n'est pas occupé :
                     if (!occupes.contains(robot)){
                         donneOrdre(robot, incendie);
+                        break; // et on arrête de chercher un robot puisqu'on l'a déjà trouvé
                     }
                 }
             }
