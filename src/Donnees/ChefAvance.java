756e4d2c46b51ae919b8d75fbaad5f6ec74119e7
Jorge Luri Vañó <jorge.luri@redarquiadigital.com>
2022-11-12 17:28:06 +0100

    Avancements sur le ChefAvance

--- src/Donnees/ChefAvance.java
+++ src/Donnees/ChefAvance.java
@@ -46,10 +46,47 @@ public class ChefAvance {
 
     }
 
+    public void gestionIncendies(Incendie incendie){
+
+        // Cherchons le robot le plus proche de l'incendie
+        boolean robotTrouve = false;
+        Robot robotAMobiliser;
+        Chemin cheminAParcourir;
+        long tempsDeplacement = Long.MAX_VALUE;
+
+        for (Robot robot : donnees.getRobots()){
+            if (!occupes.contains(robot)){
+                try {
+                    Chemin chemin = new Chemin();
+                    chemin = calculateur.dijkstra(robot.getPosition(), incendie.getPosition(), robot);
+
+                    if (chemin.getTempsChemin() < tempsDeplacement){
+                        robotTrouve = true;
+                        robotAMobiliser = robot;
+                        cheminAParcourir = chemin;
+                        tempsDeplacement = chemin.getTempsChemin();
+                    }
+                } catch (PasDeCheminException e){
+                    continue;
+                }
+            }
+        }
+
+        // Maintenant, si on a trouvé un robot, on l'envoie travailler 
+        try {
+            if (robotTrouve) cheminAParcourir.creerEvenements(this.simulateur, robotAMobiliser);
+        } catch (IllegalCheminRobotException e){
+            System.out.println(e);
+        }
+
+    }
+
     public void strategie(){
         while (!incendies_rob.isEmpty()){
             for (Incendie incendie : incendies_rob.keySet()){
-
+                if (incendies_rob.get(incendie) == null){
+                    gestionIncendies(incendie);
+                }
             }
         }
     }
