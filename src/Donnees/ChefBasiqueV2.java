030c71163eed26a31a454992ad01ac7e52572af5
Maud Bergonzoli <maud.bergonzoli@grenoble-inp.org>
2022-11-14 16:21:50 +0100

    CA MAAAARCCHHHE
    enfin que le basique mais c'est cool

--- src/Donnees/ChefBasiqueV2.java
+++ src/Donnees/ChefBasiqueV2.java
@@ -44,9 +44,20 @@ public class ChefBasiqueV2 {
     public void donneOrdre(Robot robot, Incendie incendie) throws PasDeCheminException{
         try{
             Chemin chemin = new Chemin();
-            chemin = calculateur.dijkstra(robot.getPosition(), incendie.getPosition(), robot);
+            chemin = calculateur.dijkstra(robot.getPosition(), incendie.getPosition(), robot, simulateur.getDateDernierEvenement());
             chemin.creerEvenements(this.simulateur, robot); // le robot va jusqu'à l'incendie
-            this.simulateur.ajouteEvenement(new EventIntervenir(simulateur.getDateDernierEvenement(), robot, incendie)); // TODO voir si la date est la bonne
+            if (robot.getCapacite()!= -1){ // si ce n'est pas un robot à pattes
+                for (int i = 0; i < Math.min(incendie.getLitres() / robot.getQteVersement(),
+                    robot.getReservoir() / robot.getQteVersement()); i++) 
+                {
+                    simulateur.ajouteEvenement(new EventIntervenir(simulateur.getDateDernierEvenement(), robot, incendie));
+                }
+            } else { // le robot à pattes va verser son eau
+                for (int i = 0; i < incendie.getLitres() / robot.getQteVersement(); i++) 
+                {
+                    simulateur.ajouteEvenement(new EventIntervenir(simulateur.getDateDernierEvenement(), robot, incendie));
+                }
+            }
         } catch (IllegalPathException e){
             System.out.println(e);
         }
@@ -56,42 +67,57 @@ public class ChefBasiqueV2 {
         for (Robot robot : donnees.getRobots()){
             if (!occupes.contains(robot)) {
                 if (robot.getReservoir() == 0) {
-                    this.morts.add(robot);
+                    if (!morts.contains(robot)){
+                        this.morts.add(robot);
+                    }
                     continue;
                 }
                 try {
+                    System.out.println(robot);
+                    System.out.println(incendie);
                     occupes.add(robot);
-                    incendies_rob.put(incendie, robot);
+                    incendies_rob.put(incendie, robot); //hashmap donc remplace la valeur si la clé existe déjà
                     donneOrdre(robot, incendie);
+                    break;
                 } catch (PasDeCheminException e){
                     continue; //on va prendre un autre robot
                 }
             } else {
-                if (robot.getReservoir() == 0) {
+                if (robot.getReservoir() == 0  && !morts.contains(robot)) {
                     occupes.remove(robot);
-                    this.morts.add(robot);
+                    morts.add(robot);
                 } else if (!incendies_rob.containsValue(robot)){
                     occupes.remove(robot); // Si le robot peut continuer, on l'efface
-                } 
+                }
             }
         }
     }
 
     
     public void strategie() {
-        while (!incendies_rob.isEmpty() && donnees.getRobots().length == morts.size()) { //gérer cas où les robots sont vidés
-            for (Incendie incendie : incendies_rob.keySet()) {
+        while (!incendies_rob.isEmpty() && donnees.getRobots().length != morts.size()) {
+            for (Incendie incendie : donnees.getIncendies()) {
+                if (!incendies_rob.containsKey(incendie)) {
+                    continue;
+                }
                 if (incendies_rob.get(incendie) == null) {
+                    //Si on n'a pas d'affectation
                     gestionIncendies(incendie);
                 } else {
+                    Robot robot = incendies_rob.get(incendie);
+                    if (robot.getReservoir() == 0) {
+                        occupes.remove(robot);
+                        incendies_rob.put(incendie, null);
+                        if (!morts.contains(robot)){
+                            morts.add(robot);
+                        }
+                    }
                     if (incendie.getLitres() <= 0) { // si l'incendie est éteint
-                        if (incendies_rob.get(incendie).getReservoir() > 0 || incendies_rob.get(incendie).getReservoir() == -1){
-                            occupes.remove(incendies_rob.get(incendie)); //si il reste de l'eau dans le réservoir
+                        if (robot.getReservoir() > 0 || robot.getReservoir() == -1){
+                            occupes.remove(robot); //si il reste de l'eau dans le réservoir
                         }
                         incendies_rob.remove(incendie);
-                    // } else {
-                    //     incendies_rob.put(incendie, null);
-                    }
+                    } 
                 }
             }
         } if (incendies_rob.isEmpty()) {System.out.println("Tous les incendies sont éteints\n");}
