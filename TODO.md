58a3d77f9ef4c5ee1501196c7b33abe9a2a86e26
Logan Willem <logan.willem@grenoble-inp.org>
2022-11-16 19:00:09 +0100

    Dessin des drones fini ! Je corrige mtn les bugs avant de m'attaquer à la création de la classe abstraite des chefs

--- TODO.md
+++ TODO.md
@@ -1,15 +1,16 @@
-- Implementer deux classes chef pompier avec :
-1) Méthode pour algo élémentaire
--> Structure pour voir quel robot occupé (arrayList de Robot)
--> Structure avec quel incendie pris en charge par robot i (hashMap <Incendie, Robot>)
--> donneOrdre doit ajouter la liste d'évènements pour le robot
-2) Méthode pour algo avancé
+1) Méthode pour algo avancé
 -> Méthode pour donner des ordres entre chef pompier et robots
 -> Structure pour voir quel robot occupé avec quel incendie (hashmap?)
 -> Méthode pour que un robot calcule son itinéraire le plus court
 -> Méthode pour remplir son réservoir au point le plus proche
 
 
-* Pourquoi on peut pas reset le test Basique ?
+## REPORTED BUGS:
+* On ne peut pas restart les tests quand on à un chef (TestBasique.java)
+
+## BUGS BEING INVESTIGATED:
 * Pourquoi le spiralOfMadness ne passe pas du tout ?
 * Pourquoi le testBasique ne marche pas sur desertOfDeath ?
+
+## PISTE D'AMÉLIORATION:
+* 
--- src/io/Dessin.java
+++ src/io/Dessin.java
@@ -12,7 +12,6 @@ import Donnees.Robot.Robot;
 import Evenements.Simulateur;
 import gui.GUISimulator;
 import gui.ImageElement;
-import gui.Oval;
 import gui.Rectangle;
 import gui.Text;
 
@@ -30,8 +29,7 @@ public class Dessin {
         // Il faut mettre un fond dans le cas où on dessine des arbres
         for (int i = 0; i < carte.getNbLignes(); i++) {
             for (int j = 0; j < carte.getNbColonnes(); j++) {
-                Case _case = carte.getCase(i, j);
-                if (_case.getNature() == NatureTerrain.FORET)
+                if (carte.getCase(i, j).getNature() == NatureTerrain.FORET)
                     gui.addGraphicalElement(new ImageElement(tailleCase * j,
                             i * tailleCase, NatureTerrain.TERRAIN_LIBRE.getImagePath(), tailleCase, tailleCase, gui));
             }
@@ -42,19 +40,45 @@ public class Dessin {
                 NatureTerrain current_case_nature = carte.getCase(i, j).getNature();
                 gui.addGraphicalElement(new ImageElement(tailleCase * j,
                         i * tailleCase, current_case_nature.getImagePath(), tailleCase, tailleCase, gui));
-                // Si c'est une forêt, on créé un effet de densité quand plusieurs forêt sont
-                // collées
+                // Si c'est une forêt, effet de densité si plusieurs forêts sont collées
                 if (current_case_nature == NatureTerrain.FORET) {
                     if (j + 1 < carte.getNbColonnes() && carte.getCase(i, j + 1).getNature() == NatureTerrain.FORET) {
                         gui.addGraphicalElement(new ImageElement(tailleCase * j + tailleCase / 2,
                                 i * tailleCase, current_case_nature.getImagePath(), tailleCase, tailleCase, gui));
                     }
                     if (i + 1 < carte.getNbLignes() && carte.getCase(i + 1, j).getNature() == NatureTerrain.FORET) {
-                        gui.addGraphicalElement(new ImageElement(tailleCase * j,
-                                i * tailleCase + tailleCase / 2, current_case_nature.getImagePath(), tailleCase,
-                                tailleCase, gui));
+                        gui.addGraphicalElement(new ImageElement(tailleCase * j, i * tailleCase + tailleCase / 2,
+                                current_case_nature.getImagePath(), tailleCase, tailleCase, gui));
                     }
                 }
+            }
+        }
+    }
+
+    /**
+     * Fonction permettant de dessiner l'ensemble des robots
+     * 
+     * @param robots     : Liste des robots à dessiner
+     * @param gui        : Interface graphique sur laquelle dessiner les cases
+     * @param tailleCase : Taille des cases telles qu'elles doivent apparaître sur
+     *                   le dessin (non pas la taille fictive des cases)
+     */
+    private static void dessinRobots(Robot[] robots, GUISimulator gui, int tailleCase) {
+        for (Robot robot : robots) {
+            Case pos = robot.getPosition();
+            int i = pos.getLigne();
+            int j = pos.getColonne();
+
+            gui.addGraphicalElement(
+                    new ImageElement(j * tailleCase, i * tailleCase, robot.getImagePath(), tailleCase,
+                            tailleCase, gui));
+
+            // Dessin de la jauge
+            if (robot.getReservoir() != 0) {
+                float pourcentageReservoir = (float)robot.getReservoir() / robot.getCapacite();
+                gui.addGraphicalElement(new Rectangle(tailleCase / 2 + tailleCase * j + 3 * tailleCase / 10,
+                        tailleCase * i + tailleCase / 2 + (int) ((tailleCase / 4) * (1 - pourcentageReservoir)),
+                        Color.BLUE, Color.BLUE, 3, (int) (tailleCase / 2 * pourcentageReservoir)));
             }
         }
     }
@@ -73,43 +97,12 @@ public class Dessin {
                 Case pos = incendie.getPosition();
                 int i = pos.getLigne();
                 int j = pos.getColonne();
-                gui.addGraphicalElement(new ImageElement((int)(tailleCase * j + tailleCase / 2 * (1 - (float)incendie.getLitres() / incendie.getLitresInit())),
-                        (int)(i * tailleCase + tailleCase / 2 * (1 - (float)incendie.getLitres() / incendie.getLitresInit())), "assets/Fire.png", tailleCase * incendie.getLitres() / incendie.getLitresInit(), tailleCase * incendie.getLitres() / incendie.getLitresInit(), gui));
-                // gui.addGraphicalElement(new Oval(tailleCase / 2 + tailleCase * j,
-                // tailleCase / 2 + i * tailleCase, Color.GRAY, Incendie.getColor(), tailleCase
-                // / 2 + 10));
-                // gui.addGraphicalElement(new Text(tailleCase / 2 + tailleCase * j, tailleCase
-                // / 2 + tailleCase * i,
-                // Color.BLACK, Integer.toString(incendie.getLitres())));
+                float pourcentageExtinction = (float) incendie.getLitres() / incendie.getLitresInit();
+                gui.addGraphicalElement(new ImageElement(
+                        (int) (tailleCase * j + tailleCase / 2 * (1 - pourcentageExtinction)),
+                        (int) (i * tailleCase + tailleCase / 2 * (1 - pourcentageExtinction)), "assets/Fire.png",
+                        (int) (tailleCase * pourcentageExtinction), (int) (tailleCase * pourcentageExtinction), gui));
             }
-        }
-    }
-
-    /**
-     * Fonction permettant de dessiner l'ensemble des robots
-     * 
-     * @param robots     : Liste des robots à dessiner
-     * @param gui        : Interface graphique sur laquelle dessiner les cases
-     * @param tailleCase : Taille des cases telles qu'elles doivent apparaître sur
-     *                   le dessin (non pas la taille fictive des cases)
-     */
-    private static void dessinRobots(Robot[] robots, GUISimulator gui, int tailleCase) {
-        for (Robot robot : robots) {
-            Case pos = robot.getPosition();
-            int i = pos.getLigne();
-            int j = pos.getColonne();
-            gui.addGraphicalElement(new Rectangle(tailleCase / 2 + tailleCase * j,
-                    tailleCase / 2 + i * tailleCase, Color.GRAY, Robot.getColor(), tailleCase / 2));
-            // Dessin du texte représentant le type de robot
-            gui.addGraphicalElement(new Text(tailleCase / 2 + tailleCase * j, tailleCase / 2 + tailleCase * i,
-                    Color.WHITE, Robot.getNom(robot.getClass())));
-            // Dessin de la jauge
-            if (robot.getReservoir() != 0)
-                gui.addGraphicalElement(new Rectangle(tailleCase / 2 + tailleCase * j + 3 * tailleCase / 10,
-                        tailleCase * i + tailleCase / 2
-                                + (int) ((tailleCase / 4) * (1 - (float) robot.getReservoir() / robot.getCapacite())),
-                        Color.BLUE, Color.BLUE, 3,
-                        (int) (tailleCase / 2 * ((float) robot.getReservoir() / robot.getCapacite()))));
         }
     }
 
